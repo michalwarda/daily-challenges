a554a389069de451853dc3f6f6e2f620005f0051
Michał Warda <michal.warda@elpassion.pl>
2016-06-22 15:49:17 +0200

    Handle 11th, 12th and 13th correctly.

--- 267-easy/js_dog/src/js_dog.js
+++ 267-easy/js_dog/src/js_dog.js
@@ -1,6 +1,14 @@
 function toSpoken(number) {
-  const ending = {1: 'st', 2: 'nd', 3: 'rd'}[number % 10] || 'th';
+  const ending = convertNumber(number);
   return `${number}${ending}`;
+}
+
+function convertNumber(number) {
+  if([11, 12, 13].includes(number)) {
+    return 'th';
+  } else {
+    return {1: 'st', 2: 'nd', 3: 'rd'}[number % 10] || 'th'
+  }
 }
 
 function createList(place, length = 100) {
